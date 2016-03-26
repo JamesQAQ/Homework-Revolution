205cb33fb595f5f7a4f9b2c81eddf1a2ea4987be
Liang-Chieh Chen <luniacslime@gmail.com>
2016-03-27 04:06:42 +0900

    Add file get_all_traffic.php
    所有當日流量的 api 接口
    目前不用帳號驗證
    只有當日有資料的帳號才會顯示

---
+++ ex2/get_all_traffic.php
@@ -0,0 +1,25 @@
+<?php
+  include "config.php";
+
+  $stmt = mysqli_prepare($MYSQLI, "SELECT `username`, SUM(`acctinputoctets`), SUM(`acctoutputoctets`) FROM `radacct` WHERE date_format(`acctstarttime`, '%Y-%m-%d') = date_format(now(),'%Y-%m-%d') GROUP BY `username`");
+  my_mysqli_stmt_execute($stmt);
+  mysqli_stmt_bind_result($stmt, $username, $input, $output);
+
+  $res = array();
+
+  while (mysqli_stmt_fetch($stmt)){
+    $tmp = array();
+    if ($input == NULL)
+        $input = 0;
+    if ($output == NULL)
+        $output = 0;
+    $tmp['username'] = $username;
+    $tmp['input'] = $input;
+    $tmp['output'] = $output;
+    $tmp['total'] = $input + $output;
+    array_push($res, $tmp);
+  }
+  mysqli_stmt_close($stmt);
+
+  echo json_encode($res); 
+?>
