c968e537d57bcc9cff67b16527ce5b4ba67d70d3
Liang-Chieh Chen <luniacslime@gmail.com>
2016-03-27 02:00:13 +0900

    Update: output of get_traffic.php
    原本只有總流量
    改為進出跟總流量

--- ex2/get_traffic.php
+++ ex2/get_traffic.php
@@ -3,18 +3,20 @@
 
   if (isset($_GET['username']))
   {
-    $stmt = mysqli_prepare($MYSQLI, "SELECT SUM(`acctinputoctets` + `acctoutputoctets`) FROM `radacct` WHERE `username` = ? AND date_format(`acctstarttime`, '%Y-%m-%d') = date_format(now(),'%Y-%m-%d')");
+    $stmt = mysqli_prepare($MYSQLI, "SELECT SUM(`acctinputoctets`), SUM(`acctoutputoctets`) FROM `radacct` WHERE `username` = ? AND date_format(`acctstarttime`, '%Y-%m-%d') = date_format(now(),'%Y-%m-%d')");
     mysqli_stmt_bind_param($stmt, "s", $_GET['username']);
     my_mysqli_stmt_execute($stmt);
-    mysqli_stmt_bind_result($stmt, $traffic);
+    mysqli_stmt_bind_result($stmt, $input, $output);
     if (mysqli_stmt_fetch($stmt)){
-      echo json_encode(array("traffic"=>$traffic));
+      if ($input == NULL)
+          $input = 0;
+      if ($input == NULL)
+          $output = 0;
+      echo json_encode(array("input"=>$input, "output"=>$output, "total"=>$input + $output));
     }
     else{
       echo json_encode(array("status"=>"error"));
     }
-    mysqli_stmt_close($stmt);
-
-    
+    mysqli_stmt_close($stmt);    
   }
 ?>
