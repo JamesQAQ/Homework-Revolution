1e09d993593d59cc77d1dadeac09133b92d366bf
Liang-Chieh Chen <luniacslime@gmail.com>
2016-05-15 23:01:54 +0900

    Add file: api/create_user.php
    建立一個新的帳號，須同時指定 groupname 與 password
    是否 admin 為額外選項，若有 admin 參數將設定為 group admin

---
+++ api/create_user.php
@@ -0,0 +1,67 @@
+<?php
+  include "../config.php";
+
+  /*
+  Input:
+    groupname=family
+    &username=test
+    &password=test
+    &admin=
+  Output:
+    {
+      "status":"success"
+    }
+  */
+
+  if ($USER['groupname'] === 'admin' && isset($_POST['groupname'])
+      && isset($_POST['username']) && isset($_POST['password'])){
+
+    $stmt = mysqli_prepare($MYSQLI, "SELECT `id` FROM `radcheck` WHERE `username` = ?");
+    mysqli_stmt_bind_param($stmt, "s", $_POST['username']);
+    my_mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $id);
+    if (mysqli_stmt_fetch($stmt)){
+      echo json_encode(array("status"=>"username existed"));
+      exit();
+    }
+    mysqli_stmt_close($stmt);
+
+    $stmt = mysqli_prepare($MYSQLI, "SELECT `id` FROM `radgroupreply` WHERE `groupname` = ?");
+    mysqli_stmt_bind_param($stmt, "s", $_POST['groupname']);
+    my_mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $id);
+    if (!mysqli_stmt_fetch($stmt)){
+      echo json_encode(array("status"=>"group not exists"));
+      exit();
+    }
+    mysqli_stmt_close($stmt);
+
+    $stmt = mysqli_prepare($MYSQLI, "INSERT INTO `radcheck` (`username`, `attribute`, `op`, `value`) VALUES (?, 'Cleartext-Password', ':=', ?)");
+    mysqli_stmt_bind_param($stmt, "ss", $_POST['username'], $_POST['password']);
+    my_mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    $stmt = mysqli_prepare($MYSQLI, "INSERT INTO `radusergroup` (`username`, `groupname`) VALUES (?, ?)");
+    mysqli_stmt_bind_param($stmt, "ss", $_POST['username'], $_POST['groupname']);
+    my_mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    $stmt = mysqli_prepare($MYSQLI, "INSERT INTO `Limits` (`username`, `TrafficLimit`, `TimeLimit`) VALUES (?, 536870912, 1800)");
+    mysqli_stmt_bind_param($stmt, "s", $_POST['username']);
+    my_mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    if (isset($_POST['admin'])){
+      $stmt = mysqli_prepare($MYSQLI, "INSERT INTO `GroupAdmins` (`username`, `groupname`) VALUES (?, ?)");
+      mysqli_stmt_bind_param($stmt, "ss", $_POST['username'], $_POST['groupname']);
+      my_mysqli_stmt_execute($stmt);
+      mysqli_stmt_close($stmt);
+    }
+
+    echo json_encode(array("status"=>"success"));
+  }
+  else{
+    echo json_encode(array("status"=>"permission denied"));
+  }
+
+?>
